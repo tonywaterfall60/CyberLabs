7d6732c876539d2df740e56ac4a371d7db065fa8
tonywaterfall60 <tonywaterfall60@gmail.com>
2026-09-25 13:18:04 -0400

    Align IR case PCAP timestamps with case timeline

--- Extra Practice/11-full-incident-response-case/generate_pcap.py
+++ Extra Practice/11-full-incident-response-case/generate_pcap.py
@@ -3,7 +3,7 @@ from scapy.all import Ether, IP, UDP, TCP, DNS, DNSQR, Raw, wrpcap
 
 out = sys.argv[1] if len(sys.argv) > 1 else 'incident.pcap'
 pkts = []
-T = 1761408000
+T = 1790301600
 
 def add(pkt, offset):
     pkt.time = T + offset
