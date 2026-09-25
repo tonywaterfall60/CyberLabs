1756d4318e2929500ccf8b09f8449b7d585580fe
tonywaterfall60 <tonywaterfall60@gmail.com>
2026-09-25 13:54:48 -0400

    Deepen Intermediate reverse engineering intermediate/11-intro-to-reverse-engineering/challenge/build.sh

--- intermediate/11-intro-to-reverse-engineering/challenge/build.sh
+++ intermediate/11-intro-to-reverse-engineering/challenge/build.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
-set -e
-gcc -O0 -fstack-protector-strong -o training-bin training.c
-echo "[+] Built ./training-bin"
+set -euo pipefail
+gcc -O0 -fstack-protector-strong -fPIE -pie -o training-bin training.c
+strip training-bin
+echo "[+] Built stripped PIE binary: ./training-bin"
